9c188a5737fa09d1c09a151e6158fff6ceb65575
Orsi Nagy <orsolya.n91@gmail.com>
2019-06-14 15:22:25 +0200

    Restarted project with correct framework

---
+++ TodoApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] todoList = System.IO.File.ReadAllLines(@"C:\Test.txt");
+            for ( int i = 0; i < todoList.Length; i++)
+            {
+                Console.WriteLine(i+1 + ". " + todoList[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
