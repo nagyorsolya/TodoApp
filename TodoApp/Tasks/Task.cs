91d658228058c6ea98c2cdbab258d2203efe4d13
Orsi Nagy <orsolya.n91@gmail.com>
2019-06-15 18:04:26 +0200

    Created task class so properties can be stored per item

---
+++ TodoApp/Tasks/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Tasks
+{
+  public class Task
+    {
+       public string name;
+       public bool isCompleted = false;
+        public Task(string name, bool isCompleted)
+        {
+            this.name = name;
+            this.isCompleted = isCompleted;
+        }
+    }
+}
